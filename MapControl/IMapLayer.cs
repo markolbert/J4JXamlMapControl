5e09aef506425ff3283e94b944c9f6be38ec3d79
Mark Olbert <mark@arcabama.com>
2022-04-21 19:24:15 -0700

    move to separate file

---
+++ MapControl/IMapLayer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.UI.Xaml.Media;
+
+namespace J4JSoftware.XamlMapControl;
+
+public interface IMapLayer : IMapElement
+{
+    Brush? MapBackground { get; }
+    Brush? MapForeground { get; }
+}
