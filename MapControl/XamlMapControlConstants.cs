3d8743a4968a5ffc6b3831c52bf7ecba2d7e115d
Mark Olbert <mark@arcabama.com>
2022-04-21 19:25:03 -0700

    make double comparison tolerance explicit

---
+++ MapControl/XamlMapControlConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace J4JSoftware.XamlMapControl;
+
+public static class XamlMapControlConstants
+{
+    public const double ScaleTolerance = 0.0001;
+    public const double ZoomTolerance = 0.0001;
+    public const double HeadingTolerance = 0.0001;
+}
