e94666ee40fafe0ffd1113514f35cf4a32216ea5
Mark Olbert <mark@arcabama.com>
2022-04-21 17:26:54 -0700

    conform nullability

---
+++ MapControl/IMapElement.cs
@@ -0,0 +1,9 @@
+﻿namespace J4JSoftware.XamlMapControl;
+
+/// <summary>
+/// Optional interface to hold the value of the attached property MapPanel.ParentMap.
+/// </summary>
+public interface IMapElement
+{
+    MapBase? ParentMap { get; set; }
+}
